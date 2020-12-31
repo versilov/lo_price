628eef07e701c973fb23c6a6f98e393ebc55365e
Stas Versilov <stas.versilov@gmail.com>
2020-12-31 16:55:03 +0400

    Don't show slash, if no unit

--- lib/bot.ex
+++ lib/bot.ex
@@ -208,7 +208,7 @@ defmodule LoPrice.Bot do
 
   def notify_about_price_change(chat_id, product_name, store_name, price, unit \\ nil, product_url, image_url) do
     caption =
-      "#{product_name}@#{store_name} — #{Product.format_price(price)}/#{unit || ""}\n#{product_url}"
+      "#{product_name}@#{store_name} — #{Product.format_price(price)}#{unit && "/" <> unit || ""}\n#{product_url}"
 
     ExGram.send_photo(chat_id, image_url, caption: caption, bot: @bot)
   end
